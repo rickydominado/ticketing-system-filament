87b93f6381719e24be413c3171bee12e0f19ab1c
Ricky Dominado <dominado.ricky@gmail.com>
2022-11-11 13:47:20 +0800

    Modify User Model and AuthServiceProvider
    * Added canAccessFillament() function in User Model
    
    * Renamed 'Super Admin' Role to 'super-admin' in AuthServiceProvider
    
    * Publish SpatieLaravelPermission config file

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
@@ -43,4 +45,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function canAccessFilament(): bool
+    {
+        return $this->hasAnyRole([
+            'super-admin',
+            'admin',
+            'agent',
+        ]) && $this->hasVerifiedEmail();
+    }
 }
